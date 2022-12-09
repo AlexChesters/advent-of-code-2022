21491ae05801b5d4523423dea5489e53fc16dfe0
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-12-09 12:41:06 +0000

    finish day 2

--- 02/main.py
+++ 02/main.py
@@ -1,34 +1,45 @@
-# First column - opponent's move
-# A = rock
-# B = paper
-# C = scissors
-
-# Second column - what you should play in response
-# X = rock
-# Y = paper
-# Z = scissors
-
-# The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
-# plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
-
 import os
 
 shape_score = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3
+    "A": 1,
+    "B": 2,
+    "C": 3
 }
 
 outcome_score = {
-    "loss": 0,
-    "draw": 3,
-    "win": 6
+    "X": 0,
+    "Y": 3,
+    "Z": 6
 }
 
-move_map = {
-    "A": "X",
-    "B": "Y",
-    "C": "Z"
+outcome_map = {
+    # rock
+    "A": {
+        # lose - scissors
+        "X": "C",
+        # draw - rock
+        "Y": "A",
+        # win - paper
+        "Z": "B"
+    },
+    # paper
+    "B": {
+        # lose - rock
+        "X": "A",
+        # draw - paper
+        "Y": "B",
+        # win - scissors
+        "Z": "C"
+    },
+    # scissors
+    "C": {
+        # lose - paper
+        "X": "B",
+        # draw - scissors
+        "Y": "C",
+        # win - rock
+        "Z": "A"
+    }
 }
 
 with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r", encoding="utf-8") as f:
@@ -37,34 +48,13 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r", encoding="u
 total_score = 0
 
 for turn in strategy_guide:
-    opponent_move, best_response = turn.split(" ")
+    opponent_move, desired_outcome = turn.split(" ")
 
-    if best_response == move_map[opponent_move]:
-        total_score += (3 + shape_score[best_response])
-        continue
+    player_choice = outcome_map[opponent_move][desired_outcome]
 
-    if opponent_move == "A":
-        if best_response == "Y":
-            total_score += (6 + shape_score[best_response])
-
-        if best_response == "Z":
-            total_score += (0 + shape_score[best_response])
-        continue
-
-    if opponent_move == "B":
-        if best_response == "Z":
-            total_score += (6 + shape_score[best_response])
-
-        if best_response == "X":
-            total_score += (0 + shape_score[best_response])
-        continue
-
-    if opponent_move == "C":
-        if best_response == "X":
-            total_score += (6 + shape_score[best_response])
-
-        if best_response == "Y":
-            total_score += (0 + shape_score[best_response])
-        continue
+    shape = shape_score[player_choice]
+    outcome = outcome_score[desired_outcome]
+    round_score = (shape + outcome)
+    total_score += round_score
 
 print(f"total score: {total_score}")
