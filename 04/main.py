a68dd375427bb035d600c681c6c83c886b8ccd47
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-12-09 14:06:02 +0000

    day 4 part 2

--- 04/main.py
+++ 04/main.py
@@ -3,15 +3,15 @@ import os
 with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r", encoding="utf-8") as f:
     section_assignment_pairs = f.read().splitlines()
 
-def generate_set(start, end):
-    result = set()
+def generate_list(start, end):
+    result = []
 
     for i in range(int(start), int(end) + 1):
-        result.add(i)
+        result.append(i)
 
     return result
 
-number_of_subsets = 0
+number_of_overlaps = 0
 
 for pairs in section_assignment_pairs:
     first_pair, second_pair = pairs.split(",")
@@ -19,10 +19,12 @@ for pairs in section_assignment_pairs:
     first_start, first_end = first_pair.split("-")
     second_start, second_end = second_pair.split("-")
 
-    first_set = generate_set(first_start, first_end)
-    second_set = generate_set(second_start, second_end)
+    first_list = generate_list(first_start, first_end)
+    second_list = generate_list(second_start, second_end)
 
-    if first_set.issubset(second_set) or second_set.issubset(first_set):
-        number_of_subsets += 1
+    overlaps = set(first_list).intersection(second_list)
 
-print(f"total number of subsets: {number_of_subsets}")
+    if overlaps:
+        number_of_overlaps += 1
+
+print(f"total number of overlaps: {number_of_overlaps}")
